12e1cb80ac1354f375e4fcb07ab81f4aa41b50c4
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-02-29 11:41:00 -0800

    review-image-delete

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,18 +1,36 @@
 const express = require('express')
+const { Spot, SpotImage, Review, User, Booking, ReviewImage } = require('../../db/models')
+const { requireAuth, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation')
+const { Op } = require('sequelize');
+const { check } = require('express-validator');
 
-const { Review, ReviewImage } = require('../../db/models');
-const { requireAuth } = require('../../utils/auth');
+const router = express.Router()
+router.delete('/:imageId', requireAuth, async (req, res)=>{
+    const { imageId } = req.params
+    const reviewImg = await ReviewImage.findByPk(imageId)
+    if(!reviewImg){
+        return res.status(404).json({
+            message:"Review Image couldn't be found"
+        })
+    }
+    const review = await Review.findByPk(reviewImg.reviewId)
+    if(!review){
+        return res.status(403).json({
+            message:"Forbidden"
+        })
+    }
+     if(req.user.id !== review.userId){
+        return res.status(403).json({
+            message:"Forbidden"
+        })
+    }
+    await reviewImg.destroy()
 
-const router = express.Router();
-
-
-
-
-
-
-
-
-
+    res.json({
+        message:"Successfully deleted"
+    })
+})
 
 
 module.exports = router;
