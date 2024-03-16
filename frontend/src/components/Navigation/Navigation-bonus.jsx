97148f7daa3f9e54167aaddef32a5cf2b5e8924d
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-03-16 01:40:11 -0700

    sign-up form and navigation css

--- frontend/src/components/Navigation/Navigation-bonus.jsx
+++ frontend/src/components/Navigation/Navigation-bonus.jsx
@@ -7,25 +7,6 @@ import './Navigation-bonus.css'
 function Navigation({ isLoaded }) {
   const currentUser = useSelector((state) => state.session.user);
 
-  // return (
-  //   <header className='Navigation-Header'>
-  //     <nav className='Navigation-Content'>
-  //       <NavLink to="/" className='Navigation-HomeLogo'>
-  //         <GiArrowWings size={30} />
-  //       </NavLink>
-  //       <div className='Navigation-Links'>
-  //         {currentUser && (
-  //           <NavLink to='/spots/new' className='Navigation-CreateSpot'>Host Your Spot</NavLink>
-  //         )}
-  //         <div className='Navigation-ProfileLink'>
-  //           {isLoaded && (
-  //             <ProfileButton user={currentUser} />
-  //           )}
-  //         </div>
-  //       </div>
-  //     </nav>
-  //   </header>
-  // );
   return (
     <div className='TopNavContainer'>
       <NavLink to="/" className='HomeLogoLink'>
