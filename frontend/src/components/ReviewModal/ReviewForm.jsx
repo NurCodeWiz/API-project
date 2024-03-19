cac364cd4f3ba41b35e7426db661a531a7aa384c
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-03-19 14:48:23 -0700

    star rating is done

--- frontend/src/components/ReviewModal/ReviewForm.jsx
+++ frontend/src/components/ReviewModal/ReviewForm.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux';
-// import { useModal } from '../../context/Modal';
+import { useModal } from '../../context/Modal';
 import { submitReviewForSpot} from '../../store/reviews';
-import StarRating from './Rating';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchSpecificSpot } from '../../store/spots';
@@ -10,15 +9,16 @@ import './ReviewForm.css'
 const ReviewForm = ({ spotId, onReviewUpdate }) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [stars, setStars] = useState(null)
+    const [hover, setHover] = useState(null)
+    const [validations, setValidations] = useState({})
+    const { closeModal } = useModal()
+    const [submitted, setSubmitted] = useState(false)
+    const [review, setReview] = useState('')
     const [reviewText, setReviewText] = useState('');
-    const [starRating, setStarRating] = useState(0);
+
     const [errors, setErrors] = useState([]);
 
-
-    // Function to handle star rating click
-  const handleStarRating = (rating) => {
-    setStarRating(rating);
-  };
 
   useEffect(() => {
     if (reviewText.length < 10) {
@@ -28,29 +28,70 @@ const ReviewForm = ({ spotId, onReviewUpdate }) => {
     }
   }, [reviewText]);
 
-  const handleSubmit = async (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
-    if (errors.length === 0 && starRating > 0) {
-      const review = { review: reviewText, stars: starRating };
-      await dispatch(submitReviewForSpot(spotId, review));
-      dispatch(fetchSpecificSpot(spotId)); // refresh the spot details
-      if (onReviewUpdate) onReviewUpdate(); //callback for parent component
-      navigate(`/spots/${spotId}`);
+    setSubmitted(true);
+    const formData = {
+        review,
+        stars
+    }
+
+
+    setErrors([]);
+
+    if (stars > 0) {
+      const reviewData = { review: reviewText, stars: stars };
+
+      try {
+        await dispatch(submitReviewForSpot(spotId, reviewData));
+        dispatch(fetchSpecificSpot(spotId));
+        if (onReviewUpdate) {
+          onReviewUpdate();
+        }
+        navigate(`/spots/${spotId}`);
+      } catch (error) {
+
+        console.error('Error submitting the review:', error);
+        setErrors(prevErrors => [...prevErrors, "Review already exists for this spot"]);
+      }
+    } else {
+      setErrors(prevErrors => [...prevErrors, "Please select a star rating."]);
     }
   };
   return (
     <form onSubmit={handleSubmit} className="review-form">
+        <h1 className='title'>How was your stay?</h1>
+        {"message" in validations && <p>{validations.message}</p>}
       <textarea
+        className="review-textarea"
+        type='text'
+        name='description'
         value={reviewText}
         onChange={(e) => setReviewText(e.target.value)}
         placeholder="Share your experience..."
-        className="review-textarea"
+
       />
       {errors.map((error, index) => (
         <div key={index} className="form-error">{error}</div>
       ))}
-      <StarRating currentRating={starRating} onStarSelected={handleStarRating} />
-      <button type="submit" disabled={errors.length > 0 || starRating === 0}>
+      <div className='StarsBox'>
+                {[1, 2, 3, 4, 5].map((star, i) => {
+                    const ratingValue = i + 1
+                    return (
+                        <label key={i}>
+                            <span
+                                className='Stars'
+                                onClick={() => setStars(ratingValue)}
+                                onMouseEnter={() => setHover(ratingValue)}
+                                onMouseLeave={() => setHover(null)}
+                            >
+                                {ratingValue <= (hover || stars) ? '★' : '☆'}
+                            </span>
+                        </label>
+                    )
+                })}
+            </div>
+      <button type="submit" disabled={errors.length > 0 || stars === 0}>
         Submit Review
       </button>
     </form>
